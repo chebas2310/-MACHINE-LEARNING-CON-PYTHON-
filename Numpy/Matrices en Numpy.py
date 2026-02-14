d0d1b6161332fa9ae177f6881b30414cb1056dc0
Sebas2310 <bastian.alvarado2310@gmail.com>
2026-02-14 16:21:17 -0500

    Add Numpy matrix creation example in Spanish

---
+++ Numpy/Matrices en Numpy.py
@@ -0,0 +1,19 @@
+"""
+Matrices en Numpy:
+- Todos los elementos deben ser del mismo tipo
+- Una vez creada el tamaño de la matriz no se puede cambiar
+- Debe ser de forma rectangular, no irregular
+"""
+
+#Para crear un matriz primero importamos el módulo de numpy
+import numpy as np
+
+#Para crear una matriz lo hacemos con (np.array)
+arr = np.array([1,2,3,4,5,6])
+print(arr)
+
+print("----------------------")
+
+#También podemos crear matrices con varias dimensiones de la sigueinte forma:
+brr = np.array([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]])
+print(brr)
