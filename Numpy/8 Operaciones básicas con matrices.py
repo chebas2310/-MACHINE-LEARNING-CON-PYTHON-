c851457356e28d7adfb07ccf15353301c052d525
Sebas2310 <bastian.alvarado2310@gmail.com>
2026-02-16 21:36:17 -0500

    Create 8 Operaciones básicas con matrices.py

---
+++ Numpy/8 Operaciones básicas con matrices.py
@@ -0,0 +1,7 @@
+#OPERACIONES BÁSICAS DE MATRICES CON NUMPY
+"""
+En este capítulo se cubriran las operaciones básicas que se pueden hacer con matrices en Numpy
+incluyendo, suma, resta, multiplicacion, etc. Las operaciones con matrices en Numpy funcionan igual
+que las operaciones que se pueden realizar de forma matemática.
+"""
+
