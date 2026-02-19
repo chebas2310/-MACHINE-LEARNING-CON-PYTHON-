7aeb280055def0127da9d32c91931575c3c258ee
Sebas2310 <bastian.alvarado2310@gmail.com>
2026-02-18 22:46:00 -0500

    Add basic methods overview in Pandas script
    Added a header and description for basic Pandas methods.

--- Pandas/2 Exploración inicial (Métodos).py
+++ Pandas/2 Exploración inicial (Métodos).py
@@ -1,1 +1,10 @@
+#METODOS BASICOS EN PANDAS
+"""
+Asi como en excel podemos utilizar ciertas formulas para filtrar datos en tablas o realizar operaciones estadísticas y matemáticas, de la misma forma
+podemos aplicarlo en Pandas, en este capítulo se desarrollaran los siguientes métodos básicos: 
+- (.max)
+- (.describe)
+- (.head)
+- (.tail)
+"""
 
